718dd3e64678f77503fe54218c5205d239a0a537
argierdfj <argierincon@gmail.com>
2020-08-25 10:45:03 -0500

    mostrando tabla de usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './assets/styles/Global.sass';
 import Layout from './components/layout/Layout';
-import Login from './components/containers/Login'
+// import AdminOpc from '../src/views/adminOpc/AdminOpc';
+// import Login from './views/Login/Login';
+import AllUsersTable from './components/allUsersTable/All-users-table.jsx';
 
 const App = () => {
   return (
-    <>
-      <Layout>
-        <Login />
-      </Layout>
-    </>
+    <Layout>
+      <AllUsersTable />
+    </Layout>
   );
 };
 
