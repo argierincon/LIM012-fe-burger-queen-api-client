254304c5c396bb10868c6887fadc480ef13fbd1c
argierdfj <argierincon@gmail.com>
2020-08-25 10:42:58 -0500

    verificación del layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import './Layout.sass';
 
-import topShape from './../../assets/img/top-shape.svg'
+import topShape from './../../assets/img/top-shape.svg';
+
+import Logo from '../logo/Logo';
+import UserLogged from '../userLogged/UserLogged';
+import UserSettings from '../userSettings/UserSettings';
+import TitleView from '../titleView/TitleView';
 
 /* Componente stateless: (Componente funcional) No depende de tener un estado ni un ciclo de vida, solo presenta la lógica. Son un poco más utilizados hoy día porque trabajan con la parte funcional, es decir, son funciones y nos permiten enfocarnos unicamente en lo que necesitamos.*/
 
 const Layout = ({ children }) => {
   return (
     <section className="layout">
-      <img className="layout__top-shape" src={topShape} alt="top shape"/>
+      <Logo />
+      <UserLogged userCod="Admin" />
+      <TitleView title="Usuarios" />
+      <img className="layout__top-shape" src={topShape} alt="top shape" />
       {children}
+      {/* <UserSettings /> */}
     </section>
   );
 };
